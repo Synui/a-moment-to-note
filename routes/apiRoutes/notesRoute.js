ac9726da4311e1d17ca32aebc3ca7e6fc0b7dce7
Synui <jonathon.schoenholz@yahoo.com>
2022-01-23 09:21:22 -0500

    Added API functionality

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
+const {
+    getNotes,
+    nextId,
+    createNotes,
+    deleteNotes
+} = require('../../lib/notes');
 
+router.get('/notes', (req, res) => {
+    let note = getNotes();
+    res.json(note.notes);
+});
 
+router.post('/notes', (req, res) => {
+    const note = createNotes(req.body.note);
+    res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const params = req.params.id;
+
+    const note = deleteNotes(params);
+
+    console.log(note);
+    res.json(note);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use('/', htmlRoutes);;
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-})
+});
 
