48884a938c28510d40c73cc11786f33d93fff6cb
Synui <jonathon.schoenholz@yahoo.com>
2022-01-24 03:11:08 -0500

    Working server

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid');
 const express = require('express');
-const { notes } = require('./db/db.json');
+const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoute');
-const htmlRoutes = require('./routes/htmlRoute');
 
 // Middleware that makes certain files readily available and to not gate it behind a server endpoint
 app.use(express.static('public'));
@@ -14,10 +13,65 @@ app.use(express.urlencoded({ extended: true }));
 // Parse incoming JSON data
 app.use(express.json());
 // Routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);;
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, './db/db.json')
+    )
+});
+
+app.post('/api/notes', (req, res) => {
+    // console.info(`${req.method}`);
+
+    const { title, text } = req.body
+
+    // console.info(`${JSON.stringify(req.body)}`);
+
+    let newData = {};
+
+    if (title && text) {
+        const newNote = {
+            id: uuid.v4(),
+            title,
+            text
+        }
+
+        newData = {
+            status: 'success',
+            body: JSON.stringify(newNote),
+        };
+        fs.readFile('./db/db.json', function (err, data) {
+            if (err) throw err
+
+            const parsedData = JSON.parse(data);
+
+            parsedData.push(newNote);
+
+            fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 2), function(err) {
+                if (err) throw err
+
+                console.log('Success')
+            });
+        });
+        return res.json(newData)
+    }
+    else {
+        res.json(`ERROR ${JSON.stringify(req.body)}`);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
-
